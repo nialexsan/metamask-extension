877cdd7be39ef1dae090738f18df2916572d4f25
Jongsun Suh <jongsun.suh@icloud.com>
2024-11-06 06:01:35 -0500

    Remove `getFlatState` method from `ComposableObservableStore`

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -3272,11 +3272,10 @@ export default class MetamaskController extends EventEmitter {
   getState() {
     const { vault } = this.keyringController.state;
     const isInitialized = Boolean(vault);
-    const flatState = this.memStore.getFlatState();
 
     return {
       isInitialized,
-      ...sanitizeUIState(flatState),
+      ...sanitizeUIState(this.memStore.getState()),
     };
   }
 
