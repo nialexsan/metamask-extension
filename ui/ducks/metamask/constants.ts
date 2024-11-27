9033a25d29efb63b9a08c8fc2c26b294b95cd23e
Jongsun Suh <jongsun.suh@icloud.com>
2024-11-27 10:55:40 -0500

    Merge `MetamaskState` defined in `metametrics-controller` into `initialMetamaskState`

--- ui/ducks/metamask/constants.ts
+++ ui/ducks/metamask/constants.ts
@@ -1,6 +1,11 @@
-import { Preferences } from '../../../app/scripts/controllers/preferences-controller';
+import { NameType } from '@metamask/name-controller';
+import { getDefaultPreferencesControllerState } from '../../../app/scripts/controllers/preferences-controller';
 import { MemStoreControllersComposedState } from '../../../app/scripts/metamask-controller-stores';
-import { DEFAULT_AUTO_LOCK_TIME_LIMIT } from '../../../shared/constants/preferences';
+import {
+  DEFAULT_AUTO_LOCK_TIME_LIMIT,
+  ThemeType,
+} from '../../../shared/constants/preferences';
+import { LedgerTransportTypes } from '../../../shared/constants/hardware-wallets';
 
 export const initialMetamaskState: Partial<{
   [ControllerName in keyof MemStoreControllersComposedState]: Partial<
@@ -26,6 +31,7 @@ export const initialMetamaskState: Partial<{
     approvalFlows: [],
   },
   CurrencyController: {
+    currentCurrency: 'usd',
     currencyRates: {
       ETH: {
         conversionRate: null,
@@ -50,33 +56,59 @@ export const initialMetamaskState: Partial<{
     participateInMetaMetrics: null,
     dataCollectionForMarketing: null,
   },
+  NameController: {
+    names: {
+      [NameType.ETHEREUM_ADDRESS]: {},
+    },
+  },
   NetworkController: {
     selectedNetworkClientId: '',
     networkConfigurationsByChainId: {},
     networksMetadata: {},
+  },
+  NftController: {
+    allNfts: {},
   },
   OnboardingController: {
     firstTimeFlowType: null,
     completedOnboarding: false,
   },
   PreferencesController: {
+    ...getDefaultPreferencesControllerState(),
+    theme: ThemeType.os,
     useBlockie: false,
     use4ByteResolution: true,
+    useNftDetection: true,
+    useTokenDetection: true,
+    openSeaEnabled: true,
+    securityAlertsEnabled: true,
     featureFlags: {},
     currentLocale: '',
     knownMethodData: {},
-    ledgerTransportType: undefined,
+    // Ledger transport type is deprecated. We currently only support webhid
+    // on chrome, and u2f on firefox.
+    ledgerTransportType: window.navigator.hid
+      ? LedgerTransportTypes.webhid
+      : LedgerTransportTypes.u2f,
     preferences: {
+      ...getDefaultPreferencesControllerState().preferences,
       autoLockTimeLimit: DEFAULT_AUTO_LOCK_TIME_LIMIT,
       showExtensionInFullSizeView: false,
       showFiatInTestnets: false,
+      showNativeTokenAsMainBalance: true,
       showTestNetworks: false,
       smartTransactionsOptInStatus: true,
       petnamesEnabled: true,
       featureNotificationsEnabled: false,
       privacyMode: false,
       showMultiRpcModal: false,
-    } as Preferences,
+      tokenSortConfig: {
+        key: 'tokenFiatAmount',
+        order: 'dsc',
+        sortCallback: 'stringNumeric',
+      },
+      tokenNetworkFilter: {},
+    },
   },
   SignatureController: {
     unapprovedPersonalMsgs: {},
