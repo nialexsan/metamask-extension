7ad178f526290666c43b0b9b9b344907fc5614a3
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-11-29 12:26:11 -0800

    useQuoteFetchEvents - requested

--- ui/hooks/bridge/events/useQuoteFetchEvents.ts
+++ ui/hooks/bridge/events/useQuoteFetchEvents.ts
@@ -15,22 +15,26 @@ import useLatestBalance from '../useLatestBalance';
 import { SWAPS_CHAINID_DEFAULT_TOKEN_MAP } from '../../../../shared/constants/swaps';
 import { useRequestMetadataProperties } from './useRequestMetadataProperties';
 import { useRequestProperties } from './useRequestProperties';
-import { useConvertedUsdAmounts } from './useConvertedUsdAmounts';
 import { useQuoteProperties } from './useQuoteProperties';
 import { useTradeProperties } from './useTradeProperties';
+import { useConvertedUsdAmounts } from './useConvertedUsdAmounts';
 
 // This hook is used to track cross chain swaps events related to quote-fetching
 export const useQuoteFetchEvents = () => {
   const trackCrossChainSwapsEvent = useCrossChainSwapsEventTracker();
-  const { isLoading, quotesRefreshCount, quoteFetchError } =
-    useSelector(getBridgeQuotes);
+  const {
+    isLoading,
+    quotesRefreshCount,
+    quoteFetchError,
+    quotesInitialLoadTimeMs,
+  } = useSelector(getBridgeQuotes);
   const { insufficientBal, srcTokenAddress } = useSelector(getQuoteRequest);
   const fromTokenInputValue = useSelector(getFromAmount);
   const validationErrors = useSelector(getValidationErrors);
 
   const { quoteRequestProperties } = useRequestProperties();
   const requestMetadataProperties = useRequestMetadataProperties();
-  const { srcUsdAmounts } = useConvertedUsdAmounts();
+  const { usd_amount_source } = useConvertedUsdAmounts();
   const quoteListProperties = useQuoteProperties();
   const tradeProperties = useTradeProperties();
 
@@ -72,31 +76,51 @@ export const useQuoteFetchEvents = () => {
     return latestWarnings;
   }, [validationErrors]);
 
+  // Emitted when quotes are fetched for the first time for a given request
   useEffect(() => {
-    const isInitialFetch = isLoading && quotesRefreshCount === 0;
+    const isInitialFetch =
+      isLoading &&
+      quotesRefreshCount === 0 &&
+      quotesInitialLoadTimeMs === undefined;
 
-    if (quoteRequestProperties && fromTokenInputValue && srcTokenAddress) {
-      (async () => {
-        const { usd_amount_source } = await srcUsdAmounts();
-        if (isInitialFetch || quoteFetchError) {
-          trackCrossChainSwapsEvent({
-            event: quoteFetchError
-              ? // Emitted when an error is caught during fetch
-                MetaMetricsEventName.QuoteError
-              : // Emitted when quotes are fetched for the first time for a given request
-                MetaMetricsEventName.CrossChainSwapsQuotesRequested,
-            properties: {
-              ...quoteRequestProperties,
-              ...requestMetadataProperties,
-              has_sufficient_funds,
-              usd_amount_source,
-              error_message: quoteFetchError,
-            },
-          });
-        }
-      })();
+    if (
+      quoteRequestProperties &&
+      fromTokenInputValue &&
+      srcTokenAddress &&
+      isInitialFetch
+    ) {
+      trackCrossChainSwapsEvent({
+        event: MetaMetricsEventName.CrossChainSwapsQuotesRequested,
+        properties: {
+          ...quoteRequestProperties,
+          ...requestMetadataProperties,
+          has_sufficient_funds,
+          usd_amount_source,
+        },
+      });
     }
-  }, [isLoading, quoteFetchError, quotesRefreshCount]);
+  }, [isLoading, quotesInitialLoadTimeMs]);
+
+  // Emitted when an error is caught during fetch
+  useEffect(() => {
+    if (
+      quoteRequestProperties &&
+      fromTokenInputValue &&
+      srcTokenAddress &&
+      quoteFetchError
+    ) {
+      trackCrossChainSwapsEvent({
+        event: MetaMetricsEventName.QuoteError,
+        properties: {
+          ...quoteRequestProperties,
+          ...requestMetadataProperties,
+          has_sufficient_funds,
+          usd_amount_source,
+          error_message: quoteFetchError,
+        },
+      });
+    }
+  }, [quoteFetchError]);
 
   // Emitted after each time quotes are fetched successfully
   useEffect(() => {
